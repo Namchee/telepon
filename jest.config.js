edf2f89761a57f0aeca95836a06af8be03d44a5c
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-03-19 18:23:00 +0700

    fix(build): Reconfigure jest and recreate package-lock

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   verbose: true,
-  testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)',
-  ],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
 };
